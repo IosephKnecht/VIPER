2743279e91d09d4f5789ff549150790435bb8333
IosephKnecht <Andreich.mezya67@ya.ru>
2019-03-12 10:43:41 +0300

    refactoring presenter and router

--- src/main/java/com/project/iosephknecht/viper/presenter/AbstractPresenter.kt
+++ src/main/java/com/project/iosephknecht/viper/presenter/AbstractPresenter.kt
@@ -1,14 +1,11 @@
 package com.project.iosephknecht.viper.presenter
 
-import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
-import com.project.iosephknecht.viper.removeObservers
 import com.project.iosephknecht.viper.view.AndroidComponent
 
 abstract class AbstractPresenter : ViewModel(), MvpPresenter {
 
     final override var androidComponent: AndroidComponent? = null
-    private val observers = mutableListOf<LiveData<*>>()
 
     override fun attachAndroidComponent(androidComponent: AndroidComponent) {
         this.androidComponent = androidComponent
@@ -16,18 +13,5 @@ abstract class AbstractPresenter : ViewModel(), MvpPresenter {
 
     override fun detachAndroidComponent() {
         androidComponent = null
-    }
-
-    protected fun registerObservers(vararg liveData: LiveData<*>) {
-        liveData.forEach {
-            observers.add(it)
-        }
-    }
-
-    protected fun unregisterObservers() {
-        observers.forEach {
-            it.removeObservers(androidComponent!!)
-        }
-        observers.clear()
     }
 }
--- src/main/java/com/project/iosephknecht/viper/router/MvpRouter.kt
+++ src/main/java/com/project/iosephknecht/viper/router/MvpRouter.kt
@@ -3,7 +3,7 @@ package com.project.iosephknecht.viper.router
 interface MvpRouter<L : MvpRouter.Listener> {
     interface Listener
 
-    fun setListener(listener: L)
+    fun setListener(listener: L?)
 
     fun onDestroy()
 }
