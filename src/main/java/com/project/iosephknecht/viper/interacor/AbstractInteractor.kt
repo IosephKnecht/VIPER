36e5c2cc8d286fddd2cc81d19be4c0c53c3075fb
aa.mezencev <aa.mezencev@tensor.ru>
2019-05-22 18:35:58 +0300

    added additional methods for rx wrap

--- src/main/java/com/project/iosephknecht/viper/interacor/AbstractInteractor.kt
+++ src/main/java/com/project/iosephknecht/viper/interacor/AbstractInteractor.kt
@@ -1,6 +1,8 @@
 package com.project.iosephknecht.viper.interacor
 
+import io.reactivex.Completable
 import io.reactivex.Observable
+import io.reactivex.Single
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.Disposable
 import io.reactivex.schedulers.Schedulers
@@ -16,12 +18,36 @@ abstract class AbstractInteractor<L : MvpInteractor.Listener> : MvpInteractor<L>
         listener = null
     }
 
-    protected fun <R, O : Observable<R>> discardResult(observable: O,
-                                                       block: (listener: L?, result: PendingResult<R>) -> Unit): Disposable {
+    protected fun <R, O : Observable<R>> discardResult(
+        observable: O,
+        block: (listener: L?, result: PendingResult<R>) -> Unit
+    ): Disposable {
         return observable.subscribeOn(Schedulers.io())
             .observeOn(AndroidSchedulers.mainThread())
             .doOnError { block(listener, PendingResult(null, it)) }
             .subscribe({ block(listener, PendingResult(it, null)) },
+                { it.printStackTrace() })
+    }
+
+    protected fun <R, O : Single<R>> discardResult(
+        observable: O,
+        block: (listener: L?, result: PendingResult<R>) -> Unit
+    ): Disposable {
+        return observable.subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .doOnError { block(listener, PendingResult(null, it)) }
+            .subscribe({ block(listener, PendingResult(it, null)) },
+                { it.printStackTrace() })
+    }
+
+    protected fun discardResult(
+        completable: Completable,
+        block: (listener: L?, throwable: Throwable?) -> Unit
+    ): Disposable {
+        return completable.subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .doOnError { block(listener, it) }
+            .subscribe({ block(listener, null) },
                 { it.printStackTrace() })
     }
 
